5204d240f9ed4bda58a9979c14a09aef9bb5001a
kocanbietbg <37239667+kocanbietbg@users.noreply.github.com>
2018-03-10 18:55:51 +0700

    Add files via upload

---
+++ Ex11.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg07032018;
+
+
+public class Ex11 {
+    private static final double radius = 7.5;
+
+    public static void main(String[] args) {
+
+        double perimeter = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Perimeter is = " + perimeter);
+        System.out.println("Area is = " + area);
+    }
+    
+}
---
+++ Ex59.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg07032018;
+
+import java.util.Scanner;
+public class Ex59 {
+     public static void main(String[] args){	
+     Scanner in = new Scanner(System.in);
+     System.out.print("Input a String: ");
+	 String line = in.nextLine();
+	 line = line.toLowerCase();
+	 System.out.println(line); 
+	 }		
+}
